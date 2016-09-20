090afd9e8c3d1e91135a787cf69b3e68e50ba941
guo2205 <guolingfeng@guolingfengdeMacBook-Pro.local>
2016-09-20 17:14:33 +0800

    no message

--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
-class test
+var canvas:HTMLCanvasElement = document.getElementById("can") as HTMLCanvasElement;
+var ctx:CanvasRenderingContext2D = canvas.getContext("2d");
+var width:number = canvas.width;
+var hight:number = canvas.height;
+var xoff = width*0.5;
+var yoff = width*0.5;
+for(var i=1;i<8;i++)
 {
-    public static num:number = 12;
+    ctx.beginPath();
+    ctx.arc(xoff,yoff,i*15,0,Math.PI*2,true);
+    ctx.closePath;
+    ctx.stroke();
 }
-alert(test.num.toString());
+alert(width);
