59b76a9b01addb4d2e847c5b71e3eefb53c5b23b
guo2205 <guolingfeng@192.168.0.103>
2016-09-23 01:54:58 +0800

    no messasge

--- app.js
+++ app.js
@@ -27,10 +27,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/tuding', tuding);
-app.use('/query',query);
-app.use('/mysql',mysql);
-app.use('/test',test);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ public/main.ts
@@ -0,0 +1,2 @@
+var htmlCanvas:HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement;
+var ctx = htmlCanvas.getContext("2d");
